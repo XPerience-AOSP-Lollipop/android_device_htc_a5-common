398525657a37b54f42951ab88625cc5b75170445
klozz <klozz707@gmail.com>
2016-09-22 09:33:11 +0000

    TWRP bringup
    Signed-off-by: klozz <klozz707@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,6 +143,27 @@ TARGET_RECOVERY_DEVICE_DIRS += device/htc/a5-common
 TARGET_RECOVERY_DEVICE_MODULES += chargeled
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+#BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+
+#TWRP config:
+DEVICE_RESOLUTION := 720x1280
+TW_NO_USB_STORAGE := true
+TW_NO_SCREEN_BLANK := true
+TW_MAX_BRIGHTNESS := 255
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_EXTERNAL_STORAGE_PATH := "/usb-otg"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
+TW_IGNORE_MT_POSITION_0 := true
+
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
 -BOARD_SEPOLICY_DIRS += device/htc/a5-common/sepolicy
--- a5-common.mk
+++ a5-common.mk
@@ -92,6 +92,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+#TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
